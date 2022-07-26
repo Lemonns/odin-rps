4743cf5e0e1782c54670d7bf42adf75fc1dbd876
Trey <104473511+Lemonns@users.noreply.github.com>
2022-07-25 20:11:51 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const choices = ["rock", "paper", "scissors"]
+
+function random_choice(options) {
+    let num = Math.floor(Math.random() * options.length)
+    return options[num]
+}
+
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return console.log('Tie')
+    }
+    if (playerChoice === "rock") {
+        if (computerChoice == "paper") {
+          return console.log("Computer Won!")
+        }
+        else {
+            return console.log("You won!")
+        }
+    }
+    if (playerChoice === "paper") {
+        if (computerChoice === "scissors") {
+            return console.log("Computer won!")
+        }
+        else {
+            return console.log("You won!")
+        }
+    }
+    if (playerChoice === "scissors") {
+        if (computerChoice == "rock") {
+            return console.log("Computer won!")
+        }
+        else {
+            return console.log("You won!")
+            
+        }
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("rock, paper or scissors").toLocaleLowerCase()
+        playRound(playerChoice, random_choice(choices))
+    }
+}
+
+game()
