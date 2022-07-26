6e4b02b7406fc6122bad1aff39b532960f341798
Trey <104473511+Lemonns@users.noreply.github.com>
2022-07-25 20:19:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"];
 
 function random_choice(options) {
-    let num = Math.floor(Math.random() * options.length)
-    return options[num]
+    let num = Math.floor(Math.random() * options.length);
+    return options[num];
 }
 
 
 function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
-        return console.log('Tie')
+        return console.log('Tie');
     }
     if (playerChoice === "rock") {
         if (computerChoice == "paper") {
-          return console.log("Computer Won!")
+          return console.log("Computer Won!");
         }
         else {
-            return console.log("You won!")
+            return console.log("You won!");
         }
     }
     if (playerChoice === "paper") {
         if (computerChoice === "scissors") {
-            return console.log("Computer won!")
+            return console.log("Computer won!");
         }
         else {
-            return console.log("You won!")
+            return console.log("You won!");
         }
     }
     if (playerChoice === "scissors") {
         if (computerChoice == "rock") {
-            return console.log("Computer won!")
+            return console.log("Computer won!");
         }
         else {
-            return console.log("You won!")
+            return console.log("You won!");
             
         }
     }
@@ -39,8 +39,8 @@ function playRound(playerChoice, computerChoice) {
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("rock, paper or scissors").toLowerCase()
-        playRound(playerChoice, random_choice(choices))
+        let playerChoice = prompt("rock, paper or scissors").toLowerCase();
+        playRound(playerChoice, random_choice(choices));
     }
 }
 
